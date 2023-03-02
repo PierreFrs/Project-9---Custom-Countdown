ec629503ddf958dab967d369a5e671369f74c69d
PierreFrs <pierrefraisse90@gmail.com>
2023-03-02 15:23:48 +0100

    JS form values

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdown-form');
+const dateEl = document.getElementById('date-picker');
+
+let countdownTitle = '';
+letcountdownDate = '';
+
+// Set Date Input Minimum With Today's Date
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
+// Take Values from Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
